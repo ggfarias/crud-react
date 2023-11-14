8b7cb3a12beb030bcaabb9ebe672f057418dea4a
Gustavo Farias <gustavocgf@gmail.com>
2023-11-13 23:55:17 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import 'semantic-ui-css/semantic.min.css';
+import Create from './components/Create';
+
+
+
+function App() {
+
+  return (
+    <div className='main'>
+      <h2 className='main-header'>React Crud Operations</h2>
+
+      <Create />
+    </div>
+  )
+}
+
+export default App
